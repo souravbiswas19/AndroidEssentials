15be16231668028493560cf14134d54d6731ff1e
Sourav Biswas <souravbiswas19april@gmail.com>
2024-01-05 12:37:09 +0530

    Android Essential Third Commit

--- app/src/main/java/com/example/androidessentials/MainActivity.kt
+++ app/src/main/java/com/example/androidessentials/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.androidessentials
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonSendData: Button;
+    private lateinit var number: EditText;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
