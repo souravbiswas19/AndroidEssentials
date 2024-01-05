099b36bd823dabc3b0514936c2cf39fd8f2d39b7
Sourav Biswas <souravbiswas19april@gmail.com>
2024-01-05 16:55:17 +0530

    Android Essential Fourth Commit

--- app/src/main/java/com/example/androidessentials/MainActivity.kt
+++ app/src/main/java/com/example/androidessentials/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.example.androidessentials
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var buttonSendData: Button;
-    private lateinit var number: EditText;
+    private lateinit var editTextNumber: EditText
+    private lateinit var btnSendNumber: Button
+    //private lateinit var txtResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        editTextNumber = findViewById(R.id.editTextNumber)
+        btnSendNumber = findViewById(R.id.btnSendNumber)
+        //txtResult = findViewById(R.id.txtResult)
 
+        btnSendNumber.setOnClickListener {
+            val number = editTextNumber.text.toString().toInt()
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("NUMBER", number)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/androidessentials/SecondActivity.kt
+++ app/src/main/java/com/example/androidessentials/SecondActivity.kt
@@ -1,4 +1,24 @@
 package com.example.androidessentials
 
-class SecondActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var txtReceivedNumber: TextView
+    private lateinit var btnMultiplyAndReturn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        txtReceivedNumber = findViewById(R.id.txtReceivedNumber)
+        btnMultiplyAndReturn = findViewById(R.id.btnMultiplyAndReturn)
+
+        val receivedNumber = intent.getIntExtra("NUMBER", 0)
+        txtReceivedNumber.text = "Received Number: $receivedNumber"
+
+    }
 }
